d7d3531a29c8655472689db6b475995f490ad34d
Steve Gordon <sgordon@hotmail.co.uk>
2018-09-14 00:46:08 +0100

    Initial commit 2

---
+++ src/MainApp/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MainApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public async Task OnGet(CancellationToken ct)
+        {
+            ct.Register(() => Console.WriteLine("Request cancelled"));
+
+            var client = new HttpClient();
+
+            Console.WriteLine("Starting");
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5001/api/values");
+
+            try
+            {
+                await client.SendAsync(request, ct);
+            }
+            catch(OperationCanceledException)
+            {
+                StatusCode(499); // client closed request
+            }
+
+            Console.WriteLine("Finishing");
+        }
+    }
+}
---
+++ src/SlowApiService/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SlowApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAsync(CancellationToken ct)
+        {
+            ct.Register(() => Console.WriteLine("API request cancelled"));
+            Console.WriteLine("Starting API GET");
+            try
+            {
+                await Task.Delay(60000, ct);
+            }
+            catch(OperationCanceledException)
+            {
+                return new StatusCodeResult(499); // client closed request
+            }
+            Console.WriteLine("Ending API GET");
+            return Ok("Hello");
+        }
+
+        [HttpGet]
+        [Route("fast")]
+        public ActionResult<string> FastGet(CancellationToken ct)
+        {
+            return Ok("Hello");
+        }
+    }
+}
